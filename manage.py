ebc1d636170ad139982a5795a9ed054cb2f362e6
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-04-06 21:57:29 +0300

    Change configuration manage.py and settings.py. Also add .production

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PhotoSnap.settings')
+    settings_module = 'PhotoSnap.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'PhotoSnap.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
