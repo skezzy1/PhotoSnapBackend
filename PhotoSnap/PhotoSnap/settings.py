44a79a5b1ab3730f9cfcf1e37e587419e893d708
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-04-06 21:52:22 +0300

    Change connection parameters in deployment.py

--- PhotoSnap/PhotoSnap/settings.py
+++ PhotoSnap/PhotoSnap/settings.py
@@ -11,11 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
+#DEBUG = os.getenv('DEBUG')
+#ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definit
