21dbb9c76116d528cd31ba7016333384c2b52a1f
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-04-19 22:39:45 +0300

    admin.py changes

--- PhotoSnap/mainpage/admin.py
+++ PhotoSnap/mainpage/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book, BookNote
+from .models import Book, BookNote, NoteStore
 
 admin.site.register(Book)
 admin.site.register(BookNote)
+admin.site.register(NoteStore)
