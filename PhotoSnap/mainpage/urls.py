618d95ca07780b4ea332502871496c41c14d01f1
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-04-19 22:05:02 +0300

    urls.py updation

--- PhotoSnap/mainpage/urls.py
+++ PhotoSnap/mainpage/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BookView, BookNoteView
+from .views import BookView, BookNoteView, NoteStorageView
 
 urlpatterns = [
     path('books/', BookView.book_list, name='book_list'),
     path('books/<int:book_id>/', BookView.book_detail, name='book_detail'),
     path('books/<int:book_id>/notes/', BookNoteView.book_notes, name='book_notes'),
+    path('notestorage/', NoteStorageView.note_list, name = 'note_list'),
+    path('notestorage/<int:note_id>', NoteStorageView.note_detail, name='note_detail'),
+    path('notestorage/<init:note_id>/notes/', NoteStorageView.bookstorage_notes, name='bookstorage_notes'),
 ]
