8faecd00cec81510633cc79585fab2fd1eee9fe5
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-04-19 22:07:05 +0300

    urls.py and serializers.py updation

--- PhotoSnap/mainpage/serializers.py
+++ PhotoSnap/mainpage/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
-from .models import Book, BookNote
-from rest_framework import status
-from rest_framework.response import Response
+from .models import Book, BookNote, BookStore
 from django.utils.translation import gettext_lazy as _ 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -12,4 +10,8 @@ class BookSerializer(serializers.ModelSerializer):
 class BookNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookNote
+        fields = '__all__'
+class NoteStorageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BookStore
         fields = '__all__'
