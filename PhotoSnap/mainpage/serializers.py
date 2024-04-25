b8d5dc4f4677ac67f86999aa0107c7193682b400
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-04-25 12:42:57 +0300

    Checkpoint for no dublication

--- PhotoSnap/mainpage/serializers.py
+++ PhotoSnap/mainpage/serializers.py
@@ -5,7 +5,10 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-
+    def validate(self, attrs):
+        if attrs['book_name'] == attrs['book_name']:
+            raise serializers.ValidationError("Book name is already taken, can't be same")
+        return super().validate(attrs)
 class BookNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookNote
