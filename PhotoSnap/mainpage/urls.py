d6a85bf6d3204aa167c33d3a6f011506f84349e1
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-04-28 15:32:48 +0300

    Correction in urls.py

--- PhotoSnap/mainpage/urls.py
+++ PhotoSnap/mainpage/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     path('books/<int:book_id>/sound', BookNoteView.as_view(), name='book_sound'),
     #books notes 
     path('books/<int:book_id>/notes/create', BookView.as_view(), name='book_note_create'),
-    path('books/<int:book_id>/notes/<int:note_id>/update', BookView.as_view(), name='book_note_update'),
-    path('books/<int:book_id>/notes/<int:note_id>/delete', BookView.as_view(), name='book_note_delete'),
-    path('books/<int:book_id>/notes/<int:note_id>/sound', BookView.as_view(), name='book_note_sound'),
+    path('books/<int:book_id>/notes/<int:book_note_id>/update', BookView.as_view(), name='book_note_update'),
+    path('books/<int:book_id>/notes/<int:book_note_id>/delete', BookView.as_view(), name='book_note_delete'),
+    path('books/<int:book_id>/notes/<int:book_note_id>/sound', BookView.as_view(), name='book_note_sound'),
     path('books/<int:book_id>/notes', BookNoteView.as_view(), name='book_notes'),
     #notes urls
-    path('notestorage/', NoteStorageView.as_view(), name='note_list'),
-    path('notestorage/create/', NoteStorageView.as_view(), name='note_create'),
+    path('notestorage', NoteStorageView.as_view(), name='note_list'),
+    path('notestorage/create', NoteStorageView.as_view(), name='note_create'),
     path('notestorage/<int:note_id>/update', NoteStorageView.as_view(), name='note_update'),
     path('notestorage/<int:note_id>/delete', NoteStorageView.as_view(), name='note_delete'),
     path('notestorage/<int:note_id>', NoteStorageView.as_view(), name='note_detail'), 
