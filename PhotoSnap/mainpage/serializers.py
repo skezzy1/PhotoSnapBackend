90560231cfbafeb3a834b493b2692e502b60cd83
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-05-04 20:47:45 +0300

    user field not required

--- PhotoSnap/mainpage/serializers.py
+++ PhotoSnap/mainpage/serializers.py
@@ -6,6 +6,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+        extra_kwargs = {
+            'user': {'required': False}
+        }
     def create(self, validated_data):
         return Book.objects.create(**validated_data)
     def validate(self, attrs):
@@ -17,10 +20,8 @@ class BookSerializer(serializers.ModelSerializer):
         return attrs
     def update(self, instance, validated_data):
         instance.book_name = validated_data.get('book_name', instance.book_name)
-        instance.user = validated_data.get('user', instance.user)
         instance.author = validated_data.get('author', instance.author)
         instance.book_image = validated_data.get('book_image', instance.book_image)
-        instance.book_time_created = validated_data.get('book_time_created', instance.book_time_created)
         instance.book_type = validated_data.get('book_type', instance.book_type)
         instance.book_category = validated_data.get('book_category', instance.book_category)
         instance.save()
@@ -30,13 +31,23 @@ class BookNoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookNote
         fields = '__all__'
+        extra_kwargs = {
+            'user': {'required': False}
+        }
     def create(self, validated_data):
         book_id = self.context['book_id'] 
         book = get_object_or_404(Book, pk=book_id)
         note = BookNote.objects.create(book=book, **validated_data)
         return note
-
+    def update(self, instance, validated_data):
+        instance.note_name = validated_data.get('note_name', instance.note_name)
+        instance.note_content = validated_data.get('note_content', instance.note_content)
+        instance.save()
+        return instance
 class NoteStorageSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoteStore
         fields = '__all__'
+        extra_kwargs = {
+            'user': {'required': False}
+        }
