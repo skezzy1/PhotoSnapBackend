1f79c787e0f81256a61746beaa6067a357dbf913
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-05-06 13:38:37 +0300

    PUT and DELETE fixed for BookNoteView

--- PhotoSnap/mainpage/views.py
+++ PhotoSnap/mainpage/views.py
@@ -2,6 +2,10 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListAPIView
+from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from .models import Book, BookNote, NoteStore
 from .serializers import BookSerializer, BookNoteSerializer, NoteStorageSerializer
@@ -60,19 +64,22 @@ class BookNoteView(APIView):
             serializer.save(user=request.user, book=book) 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, book_id, book_note_id):
+    def put(self, request, **kwargs):
+        book_note_id = kwargs.get('book_note_id')
+        book_id = kwargs.get('book_id')
+        book_note = get_object_or_404(BookNote, pk=book_note_id, user=request.user)
         book = get_object_or_404(Book, pk=book_id, user=request.user)
-        note = get_object_or_404(BookNote, pk=book_note_id, book=book)
-        serializer = NoteStorageSerializer(note, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def delete(self, request, book_id, book_note_id):
-        note = get_object_or_404(BookNote, pk=book_note_id, book__id=book_id, user=request.user)
-        note.delete()
-        return Response({"message": "Note deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        note_serializer = BookNoteSerializer(book_note, data=request.data, context={'request': request}, partial=True)
+        if note_serializer.is_valid():
+            note_serializer.save(user=request.user)
+            return Response(note_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(note_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, **kwargs):
+        book_note_id = kwargs.get('book_note_id')
+        book_note = get_object_or_404(BookNote, pk=book_note_id, user=request.user)
+        book_note.delete()
+        return Response({"message": "Book note deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
 
 class NoteStorageView(APIView):
     permission_classes = [IsAuthenticated]
@@ -100,3 +107,18 @@ class NoteStorageView(APIView):
         note = get_object_or_404(NoteStore, pk=note_id, user=request.user)
         note.delete()
         return Response({"message": "Note deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+class BookSearchView(APIView):
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['category', 'in_stock']  # Якщо ви хочете фільтрувати по конкретним полям
+    search_fields = ['title', 'author']  # Якщо ви хочете використовувати фільтр пошуку
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.query_params.get('q')
+        if query:
+            queryset = queryset.filter(title__icontains=query) | queryset.filter(author__icontains=query)
+        return queryset
