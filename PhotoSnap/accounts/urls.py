8a6fcef10f2cea04dba9c8f400595bb0db4f906c
Serhii Herasymchuk <serhiy1204@gmail.com>
2024-05-17 16:34:33 +0300

    Swagger deleted

--- PhotoSnap/accounts/urls.py
+++ PhotoSnap/accounts/urls.py
@@ -1,24 +1,7 @@
-from django.urls import path, re_path
+from django.urls import path
 from .views import SendPasswordResetEmailView, UserChangePasswordView, UserLoginView, UserProfileView, UserRegistrationView, UserPasswordResetView
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="PhotoSnap API",
-        default_version='v1',
-        description="API documentation for PhotoSnap",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@photosnap.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
-    re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
